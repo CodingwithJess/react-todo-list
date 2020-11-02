3ee93b54f661975b259afda0e6a8e28f67192e5e
CodingwithJess <thejessiebess@gmail.com>
2020-11-01 18:46:15 -0700

    deleted logo.svg and updated App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import { render } from '@testing-library/react';
 import './App.css';
+import List from "../src/List"
+import Form from "../src/Form"
 
 function App() {
-  return (
-    <div className="App">
+  render() {
+    return (
       constructor() {
         super()
         this.state = {
           itemList: [],
         }
       }
-    </div>
-  );
-
-  render() {
-    return (
       <div className="App">
         <h1>To Do List</h1>
         <List itemList={this.state.itemList} />
